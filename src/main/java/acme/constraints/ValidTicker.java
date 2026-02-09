876936d4e978dc6b38ccc08d8adab103378ae4eb
pabpergas <pabpergas@alum.us.es>
2026-02-09 09:55:31 +0100

    Initial commit: Acme-Starters-C project

---
+++ src/main/java/acme/constraints/ValidTicker.java
@@ -0,0 +1,43 @@
+/*
+ * ValidTicker.java
+ *
+ * Copyright (C) 2012-2026 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ReportAsSingleViolation
+
+@NotBlank
+@Pattern(regexp = "^[A-Z]{2}[0-9]{2}-\\w{5,10}$")
+
+public @interface ValidTicker {
+
+	// Standard validation properties -----------------------------------------
+
+	String message() default "{acme.validation.ticker.message}";
+
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+
+}
